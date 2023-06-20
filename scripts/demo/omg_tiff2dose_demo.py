34bdebe9ec4dd64289c73ab0100507d7a9a982fd
Luis Alfonso Olivares Jiménez <alfonso.cucei.udg@gmail.com>
2023-06-19 17:25:13 -0700

    Update omg_tiff2dose_demo.py
    Some comments translation

--- scripts/demo/omg_tiff2dose_demo.py
+++ scripts/demo/omg_tiff2dose_demo.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Ce script est sert à démontrer exemple d'utilisation du module tiff2dose de omg_dosimetry.
-Vous pouvez en faire une copie et l'adapter selon vos besoins.
+This script is used to demonstrate an example using of the omg_dosimetry tiff2dose module.
+You can make a copy and adapt it according to your needs.
     
-Écrit par Jean-François Cabana
+Written by 
+Jean-François Cabana
 jean-francois.cabana.cisssca@ssss.gouv.qc.ca
+
 2023-03-01
 """
 
-#%% Importer les librairies
+#%% Import libraries
 from omg_dosimetry import tiff2dose
 import os
 
-#%% Définir les informations générales
-info = dict(author = 'Demo Physicien',
+#%% Define general information
+info = dict(author = 'Demo Physicist',
             unit = 'Demo Linac',
             film_lot = 'XD_1',
             scanner_id = 'Epson 72000XL',
             date_exposed = '2023-01-24 16h',
             date_scanned = '2023-01-25 16h',
             wait_time = '24 heures',
-            notes = 'Scan en transmission @300ppp'
+            notes = 'Transmission mode, @300ppp and 16 bits/channel'
            )
 
-path = os.path.join(os.path.dirname(__file__), "files", "tiff2dose")   # Dossier racine
-path_scan = os.path.join(path, "scan",'A1A_Multi_6cm_001.tif')         # Dossier contenant les images numérisées, ou nom de fichier si le dossier contient plusieurs numérisations de films différents
+path = os.path.join(os.path.dirname(__file__), "files", "tiff2dose")   # Root folder
+path_scan = os.path.join(path, "scan",'A1A_Multi_6cm_001.tif')         # 0'
 outname = "Demo_dose"                                                  # Nom de fichier à produire (tif et pdf)
 
 #%% Définir les paramètres de conversion en dose
